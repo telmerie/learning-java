56430fa8b8e280038d3b52daf19f22dfbc928871
Tes <tes_7583@hotmail.com>
2024-01-30 13:57:46 +0100

    add pp

---
+++ LearningJava/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    private static void setName(String firstName, String lastName){
+        System.out.println(firstName + lastName);
+    }
+
+    public static void main(String[] args){
+        setName("Jane", "Doe");
+    }
+
+}
+
